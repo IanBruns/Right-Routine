2fb30f7b549529f488a00375737177c9c17b86a9
Ian Bruns <ian@rbruns.com>
2021-01-20 20:26:44 -0700

    Adding routes

---
+++ src/routes/OpeningPage/OpeningPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function OpeningPage(props) {
+    return (
+        <div className='landing-page'>
+            <div className='box'>
+                <h2>Sign up</h2>
+            </div>
+            <div className='box'>
+                <h2>Sign in</h2>
+            </div>
+            <div className='box'>
+                <h2>About</h2>
+            </div>
+        </div>
+    )
+}
