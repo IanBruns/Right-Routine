3208d5f158ee857bf877c8090be2a4d2cd5cd706
Ian Bruns <ian@rbruns.com>
2021-01-21 08:20:27 -0700

    Added links

--- src/routes/OpeningPage/OpeningPage.js
+++ src/routes/OpeningPage/OpeningPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function OpeningPage(props) {
     return (
         <div className='landing-page'>
             <div className='box'>
-                <h2>Sign up</h2>
+                <Link
+                    to='/register'
+                >
+                    <h2>Sign up</h2>
+                </Link>
             </div>
             <div className='box'>
-                <h2>Sign in</h2>
+                <Link
+                    to='/login'
+                >
+                    <h2>Sign in</h2>
+                </Link>
             </div>
             <div className='box'>
                 <h2>About</h2>
