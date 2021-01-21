70483ee92f2803b10e070c9c5b74fb18660c4c59
Ian Bruns <ian@rbruns.com>
2021-01-21 12:46:51 -0700

    added useEffect Hook to home page

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HomePage(props) {
+    const [routines, setRoutines] = useState([]);
+    useEffect(() => {
+        console.log(`Use effect for routines, which is currently at ${routines}`);
+    })
+
     return (
         <div className='HomePage'>
             <h2>Hello, what would you like to do?</h2>
