6e1990608e0c78157820dfec2bb4ea7ee1bd06c7
Ian Bruns <ian@rbruns.com>
2021-01-21 08:35:09 -0700

    first hook

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './LoginPage.css'
 
 export default function LoginPage(props) {
+    const [error, setError] = useState(null);
+
+    function handleSubmitJwtAuth(e) {
+        e.preventDefault();
+        const { user_name, password } = e.target;
+        console.log(user_name.value, password.value);
+    }
+
     return (
         <div className='sign-in'>
             <h2>Sign in</h2>
-            <form className='sign-in-form'>
+            <form className='sign-in-form'
+                onSubmit={(e) => handleSubmitJwtAuth(e)}
+            >
                 <div className='form-options'>
-                    <label htmlFor='uname'>Username</label>
+                    <label htmlFor='user_name'>Username</label>
                     <br />
-                    <input type='text' placeholder='enter Username' name='uname' required />
+                    <input type='text' placeholder='enter Username' name='user_name' required />
                 </div>
                 <div className='form-options'>
-                    <label htmlFor="psw">Password</label>
+                    <label htmlFor="password">Password</label>
                     <br />
-                    <input type="password" placeholder="Enter Password" name="psw" required />
+                    <input type="password" placeholder="Enter Password" name="password" required />
                 </div>
                 <br />
                 <button type="submit" className='myButton'>Log in</button>
@@ -22,7 +33,7 @@ export default function LoginPage(props) {
             </form>
             <p>
                 Don't have an account?
-                    <a href='#'>Sign up</a>
+                    <Link to='/register'>Sign up</Link>
             </p>
         </div>
     )
