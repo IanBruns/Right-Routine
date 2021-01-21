63dfd76179a040b61719c952298f526550cf5da0
Ian Bruns <ian@rbruns.com>
2021-01-21 14:20:55 -0700

    commenting unused variables

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import RoutinesApiService from '../../services/routines-api-service';
+// import RoutinesApiService from '../../services/routines-api-service';
 
 export default function HomePage(props) {
     // const [routines, setRoutines] = useState([]);
