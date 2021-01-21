fdb56905b7bd39cf120e840d9163ca3940fa82af
Ian Bruns <ian@rbruns.com>
2021-01-21 08:15:02 -0700

    adding logouts to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import TokenService from '../../services/token-service';
 
 function Header(props) {
+    function handleLogoutClick() {
+        TokenService.clearAuthToken();
+    }
+
+    function renderLogoutLink() {
+        return (
+            <div className='Header__logged-in'>
+                <Link
+                    onClick={handleLogoutClick()}
+                    to='/'>
+                    Logout
+        </Link>
+            </div>
+        )
+    }
+
+    function renderLoginLink() {
+        return (
+            <div className='Header__not-logged-in'>
+                <Link
+                    to='/register'>
+                    Register
+                </Link>
+                <Link
+                    to='/login'>
+                    Log in
+                </Link>
+            </div>
+        )
+    }
+
     return (
-        <h1>Right Routine</h1>
+        <nav className='Header'>
+            <h1>Right Routine</h1>
+            {TokenService.hasAuthToken()
+                ? renderLogoutLink()
+                : renderLoginLink()}
+        </nav>
     )
 }
 
