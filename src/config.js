7b9934e45caa5388016ec3c530d5d3d1ef1bc28b
Ian Bruns <ian@rbruns.com>
2021-01-21 08:06:42 -0700

    changed token name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'blogful-client-auth-token',
+    TOKEN_KEY: 'right-routine-client-auth-token',
 }
 
 export default config;
