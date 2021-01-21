c8b1571a495c0e67dc59b6aa7743ea475e78247a
Ian Bruns <ian@rbruns.com>
2021-01-21 12:18:20 -0700

    changed class to className

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage(props) {
     return (
-        <>
+        <div className='HomePage'>
             <h2>Hello, what would you like to do?</h2>
             <div className='home-options'>
                 <div className='box'>
                     <p>Add Exercise</p>
-                    <button className='myButton'>Add</button>
+                    <Link to='/home/add-workout'>
+                        <button className='myButton'>Add</button>
+                    </Link>
                 </div>
                 <div className='box'>
                     <p>Generate Workout</p>
-                    <button className='myButton'>Generate</button>
+                    <button className='myButton'>
+                        Generate
+                    </button>
                 </div>
                 <div className='box'>
                     <p>Add Workout Category</p>
                     <button className='myButton'>Add</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
