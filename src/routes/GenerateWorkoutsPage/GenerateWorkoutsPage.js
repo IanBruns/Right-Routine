f87ebc5a717ccb6802d07bff4b3f60522230718d
Ian Bruns <ian@rbruns.com>
2021-01-22 14:16:43 -0700

    GenerateWorkoutPage is for selecting

--- src/routes/GenerateWorkoutsPage/GenerateWorkoutsPage.js
+++ src/routes/GenerateWorkoutsPage/GenerateWorkoutsPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 export default function GenerateWorkoutsPage(props) {
+    const selectExercise = props.routines.map((mapRoutine, i) => {
+        return (
+            <div className='box' key={i}>
+                {mapRoutine.routine_name}
+                <br />
+                <button className='myButton'>
+                    Select
+                </button>
+            </div>
+        )
+    });
+
     return (
-        <p>is stored in the balls</p>
+        <div className='GenerateWorkoutsPage'>
+            <h2>What workout would you like to do today</h2>
+            {selectExercise}
+        </div>
     )
 }
