f7955cabf7163360b75e4701da28be0510ac5ca9
Ian Bruns <ian@rbruns.com>
2021-01-22 14:54:01 -0700

    working for production

---
+++ src/routes/AssignedWorkoutPage/AssignedWorkoutPage.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import RoutinesApiService from '../../services/routines-api-service';
+
+export default function AssignedWorkoutPage(props) {
+    const [exercises, setExercises] = useState([]);
+    useEffect(() => {
+        RoutinesApiService.getRoutineExercises(props.match.params.routine_id)
+            .then(res => {
+                setExercises(res);
+            })
+    }, [props.match.params.routine_id])
+
+    const todaysWorkout = exercises.map((exercise, i) => {
+        return (
+            <div className="exercise" key={i}>
+                <div className='exercise-text'>
+                    <p>{exercise.exercise_name}</p>
+                    <p>{exercise.exercise_description}</p>
+                </div>
+                <button className="myButton">Re-Roll</button>
+            </div>
+        )
+    })
+
+    return (
+        <>
+            <h2>Today's workout</h2>
+            {todaysWorkout}
+        </>
+    )
+}
