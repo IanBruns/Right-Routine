181a8a29491c6595828a23a62effb7e07be48dfe
Ian Bruns <ian@rbruns.com>
2021-01-22 12:18:44 -0700

    AddExercisePage using hooks for controlled component

--- src/routes/AddExercisePage/AddExercisePage.js
+++ src/routes/AddExercisePage/AddExercisePage.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddExercisePage(props) {
+    const [exercise_name, setExercise_name] = useState('');
+    const [exercise_description, setExercise_description] = useState('');
+    const [assigned_exercise, setAssigned_exercise] = useState('');
+
     const routineOptions = props.routines.map((mapRoutine, i) => {
         return <option key={i} value={mapRoutine.id}>{mapRoutine.routine_name}</option>
     })
 
     return (
         <div className='AddExercisePage'>
+
             <h2>Add an Exercise</h2>
+
             <form className='add-exercise-form'>
+
                 <div className='form-options'>
                     <label htmlFor='exercise_name'>Exercise Name</label>
                     <br />
-                    <input type='text' placeholder='Enter Exercise Name' name='exercise_name' required />
+                    <input type='text' placeholder='Enter Exercise Name' name='exercise_name'
+                        value={exercise_name} onChange={e => setExercise_name(e.target.value)} />
                 </div>
+
                 <div className='form-options'>
                     <label htmlFor='exercise_description'>Exercise Description</label>
                     <br />
-                    <input type='text' placeholder='Enter description' name='exercise_description' required />
+                    <input type='text' placeholder='Enter description' name='exercise_description'
+                        value={exercise_description} onChange={e => setExercise_description(e.target.value)} />
                 </div>
+
                 <div className='form-options'>
-                    <label htmlFor='ecat'>What Category does this belong to?</label>
+                    <label htmlFor='assigned_exercise'>What Category does this belong to?</label>
                     <br />
-                    <select id='ecat' name='ecat'>
+                    <select id='assigned_exercise' name='assgned_exercise'
+                        onChange={e => setAssigned_exercise(e.target.value)}>
                         <option value=''>-----</option>
                         {routineOptions}
                     </select>
                 </div>
-                <button className='myButton' type='submit'>Submit</button>
+
+                <button className='myButton' type='submit'>
+                    Submit
+                </button>
+
             </form>
             <button className='myButton'>Back</button>
         </div>
