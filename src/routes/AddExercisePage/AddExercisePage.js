291c2e72c1cfc65d720b96f2c5c3c4d81faa9e8e
Ian Bruns <ian@rbruns.com>
2021-01-22 10:42:30 -0700

    added options for sorting exercises into routines

--- src/routes/AddExercisePage/AddExercisePage.js
+++ src/routes/AddExercisePage/AddExercisePage.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
 export default function AddExercisePage(props) {
+    const routineOptions = props.routines.map((mapRoutine, i) => {
+        return <option key={i} value={mapRoutine.id}>{mapRoutine.routine_name}</option>
+    })
+
     return (
         <div className='AddExercisePage'>
             <h2>Add an Exercise</h2>
             <form className='add-exercise-form'>
                 <div className='form-options'>
-                    <label htmlFor='ename'>Exercise Name</label>
+                    <label htmlFor='exercise_name'>Exercise Name</label>
                     <br />
-                    <input type='text' placeholder='Enter Exercise Name' name='ename' required />
+                    <input type='text' placeholder='Enter Exercise Name' name='exercise_name' required />
                 </div>
                 <div className='form-options'>
-                    <label htmlFor='edesc'>Exercise Description</label>
+                    <label htmlFor='exercise_description'>Exercise Description</label>
                     <br />
-                    <input type='text' placeholder='Enter description' name='edesc' required />
+                    <input type='text' placeholder='Enter description' name='exercise_description' required />
                 </div>
                 <div className='form-options'>
                     <label htmlFor='ecat'>What Category does this belong to?</label>
                     <br />
                     <select id='ecat' name='ecat'>
                         <option value=''>-----</option>
-                        <option value='e1'>Example 1</option>
-                        <option value='e2'>Example 2</option>
-                        <option value='e3'>Example 3</option>
+                        {routineOptions}
                     </select>
                 </div>
                 <button className='myButton' type='submit'>Submit</button>
