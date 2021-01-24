05b5bc43f3ca1124daff08db88e099ec7eaf606a
Ian Bruns <ian@rbruns.com>
2021-01-24 12:28:59 -0700

    Registration page a controlled componenet

--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -2,34 +2,47 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function RegistrationPage(props) {
+    const [user_name, setUser_name] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className='sign-up'>
+
             <h2>Sign Up</h2>
+
             <form className='sign-up-form'>
+
                 <div className='form-options'>
                     <label htmlFor='user_name'>Username</label>
                     <br />
-                    <input type='text' placeholder='enter Username' name='uname' required />
+                    <input type='text' placeholder='enter Username' name='user_name' required
+                        value={user_name} onChange={e => setUser_name(e.target.value)} />
                 </div>
+
                 <div className='form-options'>
                     <label htmlFor="password">Password</label>
                     <br />
-                    <input type="password" placeholder="Enter Password" name="password" required />
+                    <input type="password" placeholder="Enter Password" name="password" required
+                        value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
-                <div className='form-options'>
-                    <label htmlFor="pswconf">Confirm Password</label>
-                    <br />
-                    <input type="password" placeholder="Confirm Password" name="pswconf" required />
-                </div>
-                <br />
+
                 <button type="submit" className='myButton'
-                >Log in</button>
+                    disabled={
+                        !(user_name.length > 0) ||
+                        !(password.length > 0)
+                    }>
+                    Register
+                </button>
+
                 <br />
+
                 <p>
                     Already have an account?
                     <Link to={'/login'}>Sign in</Link>
                 </p>
+
             </form>
+
         </div>
     )
 }
