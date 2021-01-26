f9bf273597ea72661886462a8a15d70b9628a85c
Ian Bruns <ian@rbruns.com>
2021-01-26 14:08:28 -0700

    Added basic themed styling for in app.css

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -3,14 +3,10 @@ import React, { useState } from 'react';
 export default function Exercise(props) {
     const [hidden, setHidden] = useState(true);
 
-    function randomizeReps() {
-        return Math.floor(Math.random() * (8 - 6 + 1) + 6);
-    }
-
     return (
         <div className="exercise"
             onClick={() => setHidden(!hidden)}>
-            <p>{`3 sets of ${randomizeReps()} ${props.name}('s)`}</p>
+            <p>{`3 sets of ${props.name}('s)`}</p>
             <p>{(!hidden && props.description)}</p>
         </div>
     )
